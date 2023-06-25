e06b3ec3a84dc6bf9c155776cd21d067b02c7bc7
shirokuma1101 <71858151+shirokuma1101@users.noreply.github.com>
2023-06-25 17:54:30 +0900

    add: BootstrapのScriptを追加

---
+++ Assets/Scripts/Bootstrap/Bootstrap.cs
@@ -0,0 +1,66 @@
+using System;
+using UnityEngine;
+using Utility.EnumEx;
+using Utility.Singleton;
+
+public class Bootstrap : SingletonPersistent<Bootstrap>
+{
+    public enum Platforms
+    {
+        Direct         = AvailableConnectionType.Direct,
+        Epic           = AvailableConnectionType.Direct | AvailableConnectionType.Epic,
+        NintendoSwitch = AvailableConnectionType.NintendoSwitch,
+        PlayStation4   = AvailableConnectionType.PlayStation4,
+        PlayStation5   = AvailableConnectionType.PlayStation5,
+        Steam          = AvailableConnectionType.Direct | AvailableConnectionType.Steam,
+        Xbox           = AvailableConnectionType.Direct | AvailableConnectionType.Xbox,
+        //todo: Android, iOS
+    }
+
+    public enum AvailableConnectionType
+    {
+        Direct         = 1 << 0,
+        Epic           = 1 << 1,
+        NintendoSwitch = 1 << 2,
+        PlayStation4   = 1 << 3,
+        PlayStation5   = 1 << 4,
+        Steam          = 1 << 5,
+        Xbox           = 1 << 6,
+        //todo: Android, iOS
+    }
+
+    [field: SerializeField]
+    public Platforms Platform { get; private set; } = Platforms.Direct;
+
+    public GameObject NetworkManager { get; private set; }
+
+    [SerializeField]
+    private GameObject networkManagerPrefab;
+
+
+    /// <summary>
+    /// Network API Initialization
+    /// </summary>
+    public void InitNetworkAPI(int platformsIndex)
+    {
+        NetworkManager = Instantiate(networkManagerPrefab);
+
+        foreach (var (e, i) in ((Platforms[])Enum.GetValues(typeof(Platforms))).Indexed())
+        {
+            if (i == platformsIndex)
+            {
+                Platform = e;
+                break;
+            }
+        }
+
+        switch (Platform)
+        {
+            case Platforms.Direct:
+                break;
+            case Platforms.Steam:
+                NetworkManager.AddComponent<SteamAPIManager>();
+                break;
+        }
+    }
+}
